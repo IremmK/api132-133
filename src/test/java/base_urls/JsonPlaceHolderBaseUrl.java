f9c8b4189e13d07dc76286f12ff6cecb1d66e1e1
Irem <irem_hayrunisa@hotmail.com>
2023-03-26 18:50:43 +0300

    second

--- src/test/java/base_urls/JsonPlaceHolderBaseUrl.java
+++ src/test/java/base_urls/JsonPlaceHolderBaseUrl.java
@@ -1,6 +1,7 @@
 package base_urls;
 
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
 
@@ -11,7 +12,7 @@ public class JsonPlaceHolderBaseUrl {
     @Before//This method will run before each @Test methods.
     public void setUp() {
 
-        spec = new RequestSpecBuilder().setBaseUri("https://jsonplaceholder.typicode.com").build();
+        spec = new RequestSpecBuilder().setContentType(ContentType.JSON).setBaseUri("https://jsonplaceholder.typicode.com").build();
 
     }
 }
