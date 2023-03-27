bcaec149c4e5018f149afd244de04b98bf078946
Irem <irem_hayrunisa@hotmail.com>
2023-03-28 02:24:26 +0300

    28 Marc API session

---
+++ src/test/java/test_data/HerOkuAppTestData.java
@@ -0,0 +1,26 @@
+package test_data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HerOkuAppTestData {
+
+    //This method will create a map for payload
+    public Map<String, Object> expectedDataMapMethod(Integer totalPrice, String firstName, String lastName,String checkin,String checkout,
+                                                     Map<String,String> bookingdatesMap, Boolean depositpaid, String additionalneeds) {
+        bookingdatesMap.put("checkin",checkin);
+        bookingdatesMap.put("checkout",checkout);
+
+        Map<String,Object> expectedData = new HashMap<>();
+        expectedData.put("firstname",firstName);
+        expectedData.put("lastname",lastName);
+        expectedData.put("totalprice",totalPrice);
+        expectedData.put("depositpaid",depositpaid);
+        expectedData.put("bookingdates",bookingdatesMap);
+        expectedData.put("additionalneeds",additionalneeds);
+
+        return expectedData;
+    }
+
+
+}
